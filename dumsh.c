bc2d78e4530c1a928138e58a2d7238ce2876ad52
edwinans <edyansari5@gmail.com>
2020-06-28 15:35:08 +0430

    core file

--- dumsh.c
+++ dumsh.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 #define RED "0;31"
 #define GREEN "0;32"
+#define BUFSZ 1024
 
-int main(int argc, char const *argv[]){
-    
-    printf("\033[%sm%s", RED, "err");
+char buf[BUFSIZ];
 
+int main(int argc, char const *argv[]){ 
+    write(STDOUT_FILENO, "\033[0;31m", strlen("\033[0;31m"));
+    // printf("\033[%sm%s", RED, "err");
+    printf("fdasf");
+    perror("r");
+
+    while(1){
+        read()
+    }
     return 0;
 }
